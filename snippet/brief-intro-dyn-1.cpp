06847249ec3935098f4bf4d2eca555255b626788
JCav <ggjcav@gmail.com>
2021-05-04 23:38:48 +0800

    上一个提交忘记改snippet了

--- snippet/brief-intro-dyn-1.cpp
+++ snippet/brief-intro-dyn-1.cpp
@@ -2,36 +2,51 @@
 using namespace std;
 
 class Base {
-    public:
-        virtual void fun() {cout << "Base" << endl;}
+public:
+    virtual void fun() { cout << "Base" << endl; }
 };
 
 class Derived : public Base {
-    public:
-        void fun() override {cout << "Derived" << endl;}
+public:
+    void fun() override { cout << "Derived" << endl; }
 };
 
 class DDerived : public Derived {
-    public:
-        void fun() override {cout << "DeDerived" << endl;}
+public:
+    void fun() override { cout << "DeDerived" << endl; }
+    void bad() { cout << "dangerous: " << a << endl; a = 10; }
+    int a = 0;
 };
 
-void run1(){
-    
+void run1() {
     Derived obj;
-    Base *ptr = &obj;
+    Base* ptr = &obj;
 
     // C style cast
-    Derived *pd = (Derived*)ptr;
+    Derived* pd = (Derived*)ptr;
     pd->fun(); // output: Derived
-    DDerived *pdd = (DDerived*)ptr;
-    pdd->fun(); // Runtime Error
+    DDerived* pdd = (DDerived*)ptr;
+    pdd->fun(); // Unexpected behavor
+    pdd->bad(); // Runtime Error
+}
+
+void run2() {
+    Derived obj;
+    Base* ptr = &obj;
 
     // dynamic_cast
+    Derived* pd = dynamic_cast<Derived*>(ptr);
+    if (pd == nullptr) { cout << "cast to Derived failed" << endl; } 
+    else pd->fun();
 
+    DDerived* pdd = dynamic_cast<DDerived*>(ptr);
+    if (pdd == nullptr) { cout << "cast to DDerived failed" << endl; } 
+    else pdd->bad();
 }
 
-int main(){
-    run1();
-    return 0;
+
+int main() {
+
+	return 0;
 }
+
